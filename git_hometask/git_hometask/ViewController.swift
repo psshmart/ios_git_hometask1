e98387f4b706fdc6a97fc335a1d36e2bbaf68cea
psshmart <65891171+psshmart@users.noreply.github.com>
2021-09-20 23:45:43 +0300

    Branch1 to dev (#1)
    * Added sum function
    
    * Added constant values

--- git_hometask/git_hometask/ViewController.swift
+++ git_hometask/git_hometask/ViewController.swift
@@ -8,11 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     var firstNumber = 80
+    let secondNumber = 10
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let sum = sumNumbers(first: firstNumber, second: secondNumber)
+        print(sum)
+    }
+
+    private func sumNumbers(first: Int, second: Int) -> Int {
+        first + second
     }
 }
 
