b50baf81d6153d0dc7c00c133545dee1ff71803c
psshmart <65891171+psshmart@users.noreply.github.com>
2021-09-20 23:52:05 +0300

    Added new string value

--- git_hometask/git_hometask/ViewController.swift
+++ git_hometask/git_hometask/ViewController.swift
@@ -12,11 +12,13 @@ class ViewController: UIViewController {
     var firstNumber = 80
     var dividend = 20
     var divider = 2
+    var word = "love"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let devisionResult = divide(dividened: dividend, divider: divider)
         print(devisionResult)
+        print(word)
     }
     
     private func divide(dividened: Int, divider: Int) -> Int {
